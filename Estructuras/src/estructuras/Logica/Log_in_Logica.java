e8d9d1513b2bc2df96b49c0fa3b90c449ffbdf88
crojasr757 <39276286+crojasr757@users.noreply.github.com>
2019-03-31 01:08:23 -0600

    Avance LOGIN

--- Estructuras/src/estructuras/Logica/Log_in_Logica.java
+++ Estructuras/src/estructuras/Logica/Log_in_Logica.java
@@ -3,20 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package estructuras;
+package estructuras.Logica;
+
+import javax.swing.JFrame;
 
 /**
  *
  * @author Caoba
  */
-public class Estructuras {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-       
-    }
+public class Log_in_Logica extends JFrame {
     
 }
